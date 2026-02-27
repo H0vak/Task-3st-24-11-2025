a665b0aeb87fa688bfe765607ab873d31381de46
H0vak <novak.anton2k8@gmail.com>
2026-02-27 19:33:12 +0200

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    long long n;
+    int k;
+
+    cout << "Введіть n та k: ";
+    cin >> n >> k;
+
+    while (k--) {
+        if (n % 10 == 0)
+            n /= 10;
+        else
+            n--;
+    }
+
+    cout << "Результат: " << n;
+    return 0;
+}
